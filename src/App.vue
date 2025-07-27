8f7367a109c2c1201bf9a07ae7a220bcfadc7764
Kenan Tomfie Bukit <119487201+khensin166@users.noreply.github.com>
2025-07-27 14:22:43 +0700

    update speed insight

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <router-view />
 
   <GlobalResponseModal />
+  <SpeedInsights />
 </template>
 
 <script setup>
 import "./charts/ChartjsConfig";
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 import GlobalResponseModal from "./components/modals/GlobalResponseModal.vue";
 </script>
